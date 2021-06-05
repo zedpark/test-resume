3d0b98c14b5e970a841c354a152863f39dc24e25
zed <zedparkk@gmail.com>
2021-06-05 15:40:53 -0700

    more testing

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Counter from './components/counter';
 
-const element = <h1>Test Resume</h1>;
-console.log(element);
-ReactDOM.render(element, document.getElementById('root'));
+ReactDOM.render(<Counter />, document.getElementById("root"));
