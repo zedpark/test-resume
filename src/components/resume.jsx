ecc045937acf6c2d8ec4777dd3aa744f132e3962
zed <zedparkk@gmail.com>
2021-06-05 15:45:15 -0700

    removed counter.jsx, now using resume.jsx

---
+++ src/components/resume.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Resume extends Component {
+    render() { 
+        return (  
+            <React.Fragment>
+                <h1>Mr. Park's Resume</h1>
+                <h2>Experience</h2>
+                <h3>Languages and Development Tools</h3>
+            </React.Fragment>
+        );
+    }
+}
+ 
+export default Resume;
--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Counter from './components/counter';
+import Resume from './components/resume';
 
-ReactDOM.render(<Counter />, document.getElementById("root"));
+ReactDOM.render(<Resume/>, document.getElementById("root"));
