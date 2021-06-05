02d75f88c074f210bc1f777404a5d2ec72d882ec
zed <zedparkk@gmail.com>
2021-06-05 15:15:27 -0700

    test change

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const element = <h1>Test Resume</h1>;
+const element = <h1>Test Resume1</h1>;
 console.log(element);
 ReactDOM.render(element, document.getElementById('root'));
