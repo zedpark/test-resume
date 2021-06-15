7bb7e433aa571bd3b6be8bcacf9677795a21bbb9
zed <zedparkk@gmail.com>
2021-06-15 12:30:58 -0700

    added background image

--- src/components/resume.jsx
+++ src/components/resume.jsx
@@ -1,13 +1,26 @@
 import React, { Component } from 'react';
+import background from "../assets/3376462.jpg";
 
 class Resume extends Component {
     render() { 
         return (  
             <React.Fragment>
-                <h1>Mr. Park's Resume</h1>
-                <h2>Experience</h2>
-                <div>
-                    <h3>Languages and Development Tools</h3>
+                <div style={{
+                    backgroundImage: `url(${background})`,
+                    backgroundPosition: 'center',
+                    backgroundSize: 'cover',
+                    backgroundRepeat: 'no-repeat',
+                    width: '100vw',
+                    height: '100vh'
+                    }}>
+                    <h1>Mr. Park's Resume</h1>
+                    <h2>Experience</h2>
+                    <div>
+                        <h3>Languages and Development Tools</h3>
+                        <h3>testing</h3>
+                    </div>
+                    <div>
+                    </div>
                 </div>
             </React.Fragment>
         );
