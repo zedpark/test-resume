558b2f87bf8323981defef74a9793cbdc0d572b6
zed <zedparkk@gmail.com>
2021-06-15 11:35:18 -0700

    Updates

--- static/js/main.766d25db.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonptest-resume"]=this["webpackJsonptest-resume"]||[]).push([[0],{4:function(e,s,t){"use strict";t.r(s);t(0);var n=t(2),o=t.n(n),r=t(3),c=Object(r.jsx)("h1",{children:"Test Resume1"});console.log(c),o.a.render(c,document.getElementById("root"))}},[[4,1,2]]]);
-//# sourceMappingURL=main.766d25db.chunk.js.map
