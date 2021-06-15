eaa17ae5fa692e6a53fec5d7623e039216a9ef6a
zed <zedparkk@gmail.com>
2021-06-15 13:12:55 -0700

    fixed incomplete background stretch

--- src/components/resume.jsx
+++ src/components/resume.jsx
@@ -8,10 +8,17 @@ class Resume extends Component {
                 <div style={{
                     backgroundImage: `url(${background})`,
                     backgroundPosition: 'center',
+                    backgroundAttachment: 'fixed',
                     backgroundSize: 'cover',
                     backgroundRepeat: 'no-repeat',
                     width: '100vw',
-                    height: '100vh'
+                    height: '100vh',
+                    minHeight: '100%',
+                    minWidth: '100%',
+                    position: 'fixed',
+                    overflowX: 'none',
+                    top: '0',
+                    left: '0',
                     }}>
                     <h1>Mr. Park's Resume</h1>
                     <h2>Experience</h2>
